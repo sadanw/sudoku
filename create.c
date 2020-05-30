3c305ccedd342f0ef49f93553e2f30a590ad09c1
Sada R. Nichols-Worley 22 <sadanw@moose.cs.dartmouth.edu>
2020-05-30 15:08:46 -0400

    separate solve and create modules

--- create.c
+++ create.c
@@ -2,3 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <sudoku.h>
+#include <create.h>
+#include <solve.h>
+
+
+
+sudoku_t* create() {
+    sudoku_t* new = malloc(sizeof(sudoku_t));
+    
+    if (new == NULL) {
+        return NULL;
+    }
+
+    // fill in diagonals
+    srand(time(NULL)); // use the time as a seed for rand()
+    fill_square(new, 0, 0);
+    fill_square(new, 3, 3);
+    fill_square(new, 6, 6);
+
+    // fill in remaining squares
+    fill_board(new);
+
+    // remove values
+    remove_num(new, 40);
+
+    return new;
+
+}
--- solve.c
+++ solve.c
@@ -2,3 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
+#include <sudoku.h>
+#include <create.h>
+#include <solve.h>
+
+bool solve(sudoku_t* sudo){
+    if (is_full(sudo)){
+        return true;
+    }
+
+
+    for (int x = 0; x < 9; x++){
+        for (int y = 0; y < 9; y++){
+            if (sudo->board[x][y] == 0){
+                for (int n = 1; n <= 9; n++) { 
+                    if (can_fit(sudo, x, y, n)) { 
+                        sudo->board[x][y] = n; 
+                        if (solve(sudo)){
+                            return true;
+                        }
+                        sudo->board[x][y] = 0; 
+                    } 
+                } 
+                return false; 
+            }
+        }
+    }
+    return false;
+}
